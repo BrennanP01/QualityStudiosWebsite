d19f72c156c05c56be9596ad75ff7cd454955c68
Bren <brennanjprice@lewisu.edu>
2021-11-20 19:56:39 -0600

    implemented authentication thats compatible with azure

--- index.js
+++ index.js
@@ -4,21 +4,76 @@ if(process.env.NODE_ENV !== 'production'){
 
 // Used express to get the file to display
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const app = express()
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 const fs = require('fs')
 const expressHandlebars = require('express-handlebars')
-const jQuery = require('jquery')
-
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
+const LocalStrategy = require('passport-local').Strategy
+
 // mongodb library
 const mongoose = require('mongoose')
 
+
 const dbURL = 'mongodb+srv://qsUser:hum2B2XhbxAg98b@cluster0.rmx4o.mongodb.net/qualityStudios-db?retryWrites=true&w=majority'
+
+const User = mongoose.model("User", new mongoose.Schema(
+   {
+      firstName: {
+          type: String,
+          required: true
+      },
+      lastName: {
+          type: String,
+          required: true
+      },
+      email: {
+          type: String,
+          required: true
+      },
+      password: {
+          type: String,
+          required: true
+      }
+  }
+))
+
+passport.use(
+   new LocalStrategy({usernameField: 'email'}, (email, attemptPassword, done) => {
+      console.log("Finding User")
+      User.findOne({email: email})
+         .then(user => {
+            if(!user){
+               console.log("No user with that email")
+               return done(null, false, {message: 'No user with that email'})
+            }
+            else{
+               try{
+                  if (bcrypt.compare(attemptPassword, user.password)) {
+                     return done(null, user)
+                  } else {
+                     console.log("Wrong password")
+                     return done(null, false, {message: "Wrong password"})
+                  }
+               }catch(err){
+                  console.log(err)
+               }
+            }
+         })
+}))
+passport.serializeUser((user,done) => done(null, user.id))
+passport.deserializeUser((id,done) => {
+   return done(null, User.findById(id))
+})
 
 console.log("Attempting connection to database...")
 mongoose.connect(dbURL)
-   .then((result)=> {
+   .then(()=> {
       console.log("Connected to database")
    })
    .catch((err) => console.log(err))
@@ -27,14 +82,21 @@ app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main'
 }))
 
-
-
 app.set('view engine', 'handlebars')
 // Gets the current directory, and adds the "public" file
 // This will display the html in the root directory of public. (no need to put it in the views folder)
 app.use(express.static(__dirname + '/public'))
-app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+   secret: process.env.SESSION_SECRET,
+   resave: false,
+   saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 //Reads staff directory file
 function readFile() {
@@ -105,8 +167,8 @@ app.get('/map',(req,res)=>{
  })
  
 
- // Forces the user to log in to schedule an appointment
- app.get('/schedule', (req,res)=>{
+  // Forces the user to log in to schedule an appointment
+  app.get('/schedule', checkAuthenticated, (req,res)=>{
    res.render('schedule')
 })
 
@@ -114,20 +176,69 @@ app.get('/map',(req,res)=>{
     res.render('about')
  })
 
- app.get('/login',(req,res)=>{
+ app.get('/login', checkNotAuthenticated,(req,res)=>{
    res.render('login')
 })
 
-app.post('/login')
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+   successRedirect: '/',
+   failureRedirect: '/login',
+   failureFlash: true
+}))
 
-app.get('/register', (req,res)=>{
+app.get('/register', checkNotAuthenticated,(req,res)=>{
    res.render('register')
 })
-app.post('/register', async (req,res)=>{
-   console.log("registered")
-   res.redirect('/login')
 
+app.post('/register', checkNotAuthenticated, (req,res)=>{
+   try{
+      // add user to the database
+      const salt = bcrypt.genSaltSync(10)
+      const hashedPassword = bcrypt.hashSync(String(req.body.password), salt)
+      var fName = String(req.body.firstName)
+      var lName = String(req.body.lastName)
+      var emai = String(req.body.email)
+
+      const userEntry = new User({
+         firstName: fName,
+         lastName: lName,
+         email: emai,
+         password: hashedPassword
+      })
+      userEntry.save()
+         .then((result)=>{
+            res.redirect('/login')
+         })
+         .catch((err)=>{
+            console.log(err)
+         })
+   }catch(e){
+      console.log(e)
+      res.redirect('/register')
+   }
 })
+
+app.delete('/logout', (req, res) => {
+   req.logOut()
+   res.redirect('/')
+})
+
+function checkAuthenticated(req, res, next) {
+   if(req.isAuthenticated()){
+      return next()
+   }else{
+      res.redirect('/login')
+   }
+}
+
+function checkNotAuthenticated(req, res, next) {
+   if(req.isAuthenticated()){
+      return res.redirect('/')
+   }else{
+      next()
+   }
+}
+
 
 app.listen(port, ()=>console.log(
    `Express started on http://localhost:${port}; ` +
