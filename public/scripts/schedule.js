b94411b01f53af64bb6234a9d495213afa5fa5ba
Jahi Stewart <jahikstewart@lewisu.edu>
2021-12-16 22:54:53 -0600

    fix all boxes checked bug

--- public/scripts/schedule.js
+++ public/scripts/schedule.js
@@ -1,7 +1,8 @@
 function updateResults() {    
     let searchItem = document.getElementById('search').value
     let checks = [document.getElementById('barber'), document.getElementById('lash technician'), document.getElementById('other')]
-    let rows = document.getElementById('schedule').rows //get container element rows from tbody.
+    let rows = Array.from(document.getElementById('schedule').rows).slice(1) //get rows but skip header rows
+    console.log(rows)
     let state = true
     let e, name, col_class
     checks.forEach( e => {
@@ -9,25 +10,24 @@ function updateResults() {
             state = false
         }
     })
-    if (searchItem == '' && state == true) {
-        for (let i = 0, len = rows.length ; i < len; i++) {     
+    if (searchItem == '' && state) {
+        for (let i = 0, len = rows.length ; i < len; i++) {  
             e = rows[i]
-            e[i].style.display = ''  
+            console.log(e)
+            e.style.display = ''  
         }
     }else{
         for (let i = 0, len = rows.length ; i < len; i++) { //Reminder: html collections don't have forEach()
             e = rows[i]
             name = e.id
-            if (name != 'header') {
-                col_class = e.cells[0].className
-                console.log(col_class)
-                if (name.slice(0, searchItem.length).toLowerCase() == searchItem.toLowerCase() && document.getElementById(col_class).checked) {
-                    e.style.display = '' 
-                }else if (document.getElementById(col_class).checked && searchItem == '') {
-                    e.style.display = '' 
-                } else {
-                    e.style.display = 'none'
-                }
+            col_class = e.cells[0].className
+            console.log(col_class)
+            if (name.slice(0, searchItem.length).toLowerCase() == searchItem.toLowerCase() && document.getElementById(col_class).checked) {
+                e.style.display = '' 
+            }else if (document.getElementById(col_class).checked && searchItem == '') {
+                e.style.display = '' 
+            } else {
+                e.style.display = 'none'
             }
         }
     }
